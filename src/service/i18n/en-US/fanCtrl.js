962d72986ff61946650c305b18991b8ef00beebe
liangshengnan <liangshengnan@inspur.com>
2019-03-07 16:33:31 +0800

    更新风扇页面

--- src/service/i18n/en-US/fanCtrl.js
+++ src/service/i18n/en-US/fanCtrl.js
@@ -2,6 +2,7 @@ export default {
   title: 'Fan Controller',
   sub_title: 'check and modify fan module',
   manual_mode: 'Manual Mode:',
+  thead_name: 'Fan Name',
   thead_status: 'Status',
   thead_rpm: 'Current Speed(rpm)',
   thead_duty: 'Duty',
--- src/views/fan/fanController.vue
+++ src/views/fan/fanController.vue
@@ -1,25 +1,23 @@
 <template>
   <view-content message="fanCtrl" @refresh="refresh">
     <a-row style="margin-bottom: 20px">
-      <a-col :span="4">
-        <span style="margin-right: 10px">{{$t('message.fanCtrl.manual_mode')}}</span>
-        <a-switch v-model="manualMode" :checkedChildren="$t('message.common.on')" :unCheckedChildren="$t('message.common.off')"/>
-      </a-col>
+      <a-form>
+        <a-col :span="24">
+          <a-form-item :label="$t('message.common.select_node')" :labelCol="{ span: 3 }" :wrapperCol="{ span: 4 }">
+            <a-select v-model="node" @change="onChassisChange">
+              <a-select-option v-for="(item, index) in nodeList" :key="index" :value="item">{{'node ' + index}}</a-select-option>
+            </a-select>
+          </a-form-item>
+        </a-col>
+      </a-form>
     </a-row>
-    <a-table :columns="columns" :dataSource="fanData" rowKey='id' size="small">
-      <span slot="status" slot-scope="status">
-        <a-icon v-if="status == 1" type="check-circle" style="color: green;font-size: 1.2em;"/>
-        <a-icon v-else type="close-circle" style="color: grey;font-size: 1.2em;"/>
-      </span>
-      <span slot="duty" slot-scope="duty">
-        {{duty + '%'}}
-      </span>
+    <a-table :columns="columns" :dataSource="currentFanInfo" rowKey='id' size="small">
       <span slot="action" slot-scope="record">
         <a-button-group>
-          <a-button :disabled="!record.status || !manualMode" @click.prevent="() => setRpm(record.id, 25)">25%</a-button>
-          <a-button :disabled="!record.status || !manualMode" @click.prevent="() => setRpm(record.id, 50)">50%</a-button>
-          <a-button :disabled="!record.status || !manualMode" @click.prevent="() => setRpm(record.id, 75)">75%</a-button>
-          <a-button :disabled="!record.status || !manualMode" @click.prevent="() => setRpm(record.id, 100)">100%</a-button>
+          <a-button >25%</a-button>
+          <a-button >50%</a-button>
+          <a-button >75%</a-button>
+          <a-button >100%</a-button>
         </a-button-group>
       </span>
     </a-table>
@@ -38,38 +36,77 @@
 import ViewContent from '@/components/viewContent'
 import { mapMutations } from 'vuex'
 
+import http from '@/service/api/http'
+import errorHandler, { getChassisInfo } from '@/service/api'
+
 export default {
   name: 'fanController',
   data () {
     return {
-      manualMode: true,
+      node: undefined,
+      nodeList: [],
+      chassisReqs: {},
+      currentFanInfo: [],
       columns: [
         { title: 'No.', dataIndex: 'id' },
-        { title: this.$t('message.fanCtrl.thead_status'), dataIndex: 'status', align: 'center', scopedSlots: { customRender: 'status' } },
+        { title: this.$t('message.fanCtrl.thead_name'), dataIndex: 'name', align: 'center' },
+        { title: this.$t('message.fanCtrl.thead_status'), dataIndex: 'health', align: 'center', scopedSlots: { customRender: 'status' } },
         { title: this.$t('message.fanCtrl.thead_rpm'), dataIndex: 'rpm', align: 'center' },
-        { title: this.$t('message.fanCtrl.thead_duty'), dataIndex: 'duty', align: 'center', scopedSlots: { customRender: 'duty' } },
         { title: this.$t('message.fanCtrl.thead_action'), align: 'center', scopedSlots: { customRender: 'action' } }
-      ],
-      fanData: [
-        { id: 0, status: 1, rpm: 3636, duty: 25 },
-        { id: 1, status: 1, rpm: 3636, duty: 25 },
-        { id: 2, status: 1, rpm: 3636, duty: 25 },
-        { id: 3, status: 0, rpm: 0, duty: 0 },
-        { id: 4, status: 0, rpm: 0, duty: 0 },
-        { id: 5, status: 0, rpm: 0, duty: 0 }
       ]
     }
   },
-  created () {
-    this.refresh()
-  },
   methods: {
     ...mapMutations(['setRefreshFlag']),
-    refresh () {
-      setTimeout(() => {
-        this.setRefreshFlag({ refreshFlag: false })
-      }, 1000)
+    async refresh () {
+      this.nodeList = []
+      this.chassisReqs = {}
+      try {
+        let resChassis = await getChassisInfo()
+        resChassis.data.Members.forEach((item) => {
+          let tmps = item['@odata.id'].split('/')
+          let name = tmps[tmps.length - 1]
+          this.nodeList.push(name)
+          this.chassisReqs[name] = `/redfish/v1/Chassis/${name}/Thermal`
+        })
+        this.node = this.nodeList[0]
+        let resThermal = await http('get', this.chassisReqs[this.node])
+        this.parseData(resThermal.data.Fans)
+      } catch (error) {
+        errorHandler(this, error, this.$t('message.sensor.get_err_msg'))
+      }
+
+      this.setRefreshFlag({ refreshFlag: false })
     },
+
+    // chassis变化回调函数
+    async onChassisChange (val) {
+      this.currentFanInfo = []
+      // this.setRefreshFlag({ refreshFlag: true })
+
+      try {
+        this.node = val
+        let resThermal = await http('get', this.chassisReqs[this.node])
+        // console.log(resThermal.data.Fans)
+        this.parseData(resThermal.data.Fans)
+      } catch (error) {
+        errorHandler(this, error, this.$t('message.sensor.get_err_msg'))
+      }
+
+      this.setRefreshFlag({ refreshFlag: false })
+    },
+
+    parseData (content) {
+      content.forEach((fan) => {
+        this.currentFanInfo.push({
+          id: fan['@odata.id'].split('/').pop(),
+          name: fan.Name,
+          rpm: fan.Reading,
+          health: fan.Status.Health
+        })
+      })
+    },
+
     setRpm (id, duty) {
       console.log(id + ', ' + duty)
     }
